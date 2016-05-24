0031d80f9432bf4c300b91c9f449db7368d000ce
joerg-liedl <j.liedl@dialogworks.de>
2016-05-24 22:09:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hinterland Magazin Downloader
+
+Dieser Downloader lädt alle Hinterland Magazine auf Linux und MacOs runter
+
+Zuerst das Repository klonen  
+`git clone https://github.com/joergi77/Hinterland_Magazin_Downloader.git`   
+  
+Zum Downloaden der PDF Magazine:  
+`sh hinterland-magazin-downloader.sh`
+  
+Alle Ausgaben gibt es kostenlos unter: [http://www.hinterland-magazin.de/archiv.php](http://www.hinterland-magazin.de/archiv.php).  
+Das Hinterland Magazin ist ein Vierteljahresmagazin vom [Bayerischen Flüchtlingsrat](http://www.fluechtlingsrat-bayern.de).  
+Bitte unterstützt die Arbeit des [Bayerischen Flüchtlingsrat](http://www.fluechtlingsrat-bayern.de) indem ihr die [Ausgaben kauft](http://www.hinterland-magazin.de/bestellen.php).
---
+++ hinterland-magazin-downloader.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# ------------------------------------------------------------------
+# [Author] joergi77 - https://github.com/joergi77
+#          downloader for all Hinterland Magazin issues
+#          they are downloadable for free under http://www.hinterland-magazin.de/
+#          or you can buy the paper issues under: http://www.hinterland-magazin.de/bestellen.php
+#          this script is under GNU GENERAL PUBLIC LICENSE v3.0
+# ------------------------------------------------------------------
+
+VERSION=0.1.0
+USAGE="Usage: sh hinterland-magazin-downloader.sh"
+
+
+if [ ! -d "issues" ]; then
+ mkdir issues
+fi
+
+i=17
+issues=30
+
+while [ $i -le $issues ]
+do
+	url17="http://www.hinterland-magazin.de/pdf/Hinterland17KLEIN.pdf"
+	url18="http://www.hinterland-magazin.de/pdf/Hinterland18Klein.pdf"
+	url2x="http://www.hinterland-magazin.de/pdf/Hinterland"$i"_klein.pdf"
+	url="http://www.hinterland-magazin.de/pdf/Hinterland"$i"_Klein.pdf"
+
+	# issue 17 has extra url
+	if [ "$i" -eq 17 ]; then
+		wget $url17 -P issues/
+	fi
+
+	# issue 18 has extra url
+	if [ "$i" -eq 18 ]; then
+		wget $url18 -P issues/		
+	fi
+
+	# issue 20 + 21 + 22 has extra url
+	if [ "$i" -eq 20 -o "$i" -eq 21 -o "$i" -eq 22 ]; then
+		wget $url2x -P issues/		
+	fi
+
+	# standard url
+	if [ "$i" -ge 23 -o "$i" -eq 19 ]; then
+		wget $url -P issues/		
+	fi
+	
+
+	i=$(( i+1 ))
+done
+
+
+
+
