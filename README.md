65852bf1c12b6b7c0e0503080c075673934e0eea
joergi77 <joergi@schenkelklopfer.de>
2016-10-09 20:12:24 +0200

    Anpassung des Downloaders an neue Struktur beim Hinterland Magazin

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Hinterland Magazin Downloader
 
-Dieser Downloader lädt alle Hinterland Magazine (ab #17) auf Linux und MacOs runter
+Dieser Downloader lädt alle Hinterland Magazine (ab #29) auf Linux und MacOs runter
+In einer früheren Version des Downloaders gab es die Version 17-30, leider sind aber die alten Ausgaben alle offline!
 
 Zuerst das Repository klonen  
 `git clone https://github.com/joergi77/Hinterland_Magazin_Downloader.git`   
-  
+
 Zum Downloaden der PDF Magazine:  
 `sh hinterland-magazin-downloader.sh`
-  
+
 Alle Ausgaben gibt es kostenlos unter: [http://www.hinterland-magazin.de](http://www.hinterland-magazin.de).  
 Das Hinterland Magazin ist ein Vierteljahresmagazin vom [Bayerischen Flüchtlingsrat](http://www.fluechtlingsrat-bayern.de).  
 Bitte unterstützt die Arbeit des [Bayerischen Flüchtlingsrat](http://www.fluechtlingsrat-bayern.de) indem ihr die [Ausgaben kauft oder noch besser: ein Abo abschließt](http://www.hinterland-magazin.de/bestellen.php).
--- hinterland-magazin-downloader.sh
+++ hinterland-magazin-downloader.sh
@@ -11,44 +11,13 @@ VERSION=0.1.0
 USAGE="Usage: sh hinterland-magazin-downloader.sh"
 
 
+
 if [ ! -d "issues" ]; then
  mkdir issues
 fi
 
-i=17
-issues=32
 
-while [ $i -le $issues ]
-do
-	url17="http://www.hinterland-magazin.de/pdf/Hinterland17KLEIN.pdf"
-	url18="http://www.hinterland-magazin.de/pdf/Hinterland18Klein.pdf"
-	url2x="http://www.hinterland-magazin.de/pdf/Hinterland"$i"_klein.pdf"
-	url="http://www.hinterland-magazin.de/pdf/Hinterland"$i"_Klein.pdf"
-
-	# issue 17 has extra url
-	if [ "$i" -eq 17 ]; then
-		wget $url17 -P issues/
-	fi
-
-	# issue 18 has extra url
-	if [ "$i" -eq 18 ]; then
-		wget $url18 -P issues/		
-	fi
-
-	# issue 20 + 21 + 22 has extra url
-	if [ "$i" -eq 20 -o "$i" -eq 21 -o "$i" -eq 22 ]; then
-		wget $url2x -P issues/		
-	fi
-
-	# standard url
-	if [ "$i" -ge 23 -o "$i" -eq 19 ]; then
-		wget $url -P issues/		
-	fi
-	
-
-	i=$(( i+1 ))
-done
-
-
-
-
+wget http://www.hinterland-magazin.de/wp-content/uploads/2016/07/hinterland-magazin-29.pdf -P issues/
+wget http://www.hinterland-magazin.de/wp-content/uploads/2016/07/hinterland-magazin-30.pdf -P issues/
+wget http://www.hinterland-magazin.de/wp-content/uploads/2016/06/hinterland-magazin-31.pdf -P issues/
+wget http://www.hinterland-magazin.de/wp-content/uploads/2016/09/hinterland-magazin-32.pdf -P issues/
